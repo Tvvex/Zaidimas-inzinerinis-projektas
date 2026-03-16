f261fc310cee64bac0c0a3b956bd6a0c475b4c8e
Tvvex <tvvex12@gmail.com>
2026-03-16 17:22:20 +0200

    Prideti skriptai

---
+++ Assets/Scipts/UI/PauseMenuScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject Menu;
+    public void OnEscape()
+    {
+        if (Menu.activeSelf==false)
+        {
+            Menu.SetActive(true);
+        }
+        else
+        {
+            Menu.SetActive(false);
+        }
+    }
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+}
